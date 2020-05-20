c13bdf89854b08a4bf1a153253d069463a572910
Andy Cheung <micro_zx@msn.cn>
2020-05-20 08:35:40 +0800

    2020.5.20 (2)

--- src/ac/adproj/mchat/web/WebStarter.java
+++ src/ac/adproj/mchat/web/WebStarter.java
@@ -26,23 +26,6 @@ import ac.adproj.mchat.listener.ServerListener;
 
 public class WebStarter implements AutoCloseable {
     private static ServerListener listener;
-    
-    private static final String chattingHTML = "<!DOCTYPE html>\r\n" + "<html>\r\n" + "<head>\r\n"
-            + "    <title>WebSocket client</title>\r\n" + "    <script>\r\n"
-            + "        var ws = new WebSocket(\"ws://127.0.0.1:8090/acmcs/wshandler\");\r\n" + "\r\n"
-            + "        ws.onopen = function(){\r\n" + "        };\r\n" + "\r\n"
-            + "        ws.onmessage = function(message){\r\n"
-            + "            document.getElementById(\"chatlog\").textContent += message.data + \"\\n\";\r\n"
-            + "        };\r\n" + "        function postToServer(){\r\n"
-            + "            ws.send(document.getElementById(\"msg\").value);\r\n"
-            + "            document.getElementById(\"msg\").value = \"\";\r\n" + "        }\r\n" + "\r\n"
-            + "        function closeConnect(){\r\n" + "            ws.close();\r\n" + "        }\r\n"
-            + "    </script>\r\n" + "</head>\r\n" + "<body>\r\n"
-            + "    <textarea id=\"chatlog\" readonly></textarea><br/>\r\n"
-            + "    <input id=\"msg\" type=\"text\" />\r\n"
-            + "    <button type=\"submit\" id=\"sendButton\" onClick=\"postToServer()\">Send!</button>\r\n"
-            + "    <button type=\"submit\" id=\"sendButton\" onClick=\"closeConnect()\">End</button>\r\n"
-            + "</body>\r\n" + "</body>\r\n" + "</html>";
 
     private Server server = null;
     private Thread serverThread;
