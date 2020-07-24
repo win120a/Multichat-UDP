8e184ede0ec5257d4a95e12794e72d9a6b06f449
Andy Cheung <micro_zx@msn.cn>
2020-07-25 00:45:27 +0800

    Add mutex in execute methods to make sure the execute method is only executed by one thread.

--- src/ac/adproj/mchat/service/CommonThreadPool.java
+++ src/ac/adproj/mchat/service/CommonThreadPool.java
@@ -1,3 +1,20 @@
+/*
+    Copyright (C) 2011-2020 Andy Cheung
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package ac.adproj.mchat.service;
 
 import java.util.concurrent.BlockingQueue;
@@ -23,6 +40,7 @@ public class CommonThreadPool {
     private static String comment = "";
     
     private static final byte[] MUTEX = new byte[1];
+    private static final byte[] MUTEX_EXECUTE = new byte[1];
 
     private static ThreadFactory threadFactory = r -> {
         synchronized (MUTEX) {
@@ -46,7 +64,9 @@ public class CommonThreadPool {
      * @param r 线程执行体
      */
     public static synchronized void execute(Runnable r) {
-        threadPool.execute(r);
+        synchronized (MUTEX_EXECUTE) {
+            threadPool.execute(r);
+        }
     }
     
     /**
@@ -56,11 +76,13 @@ public class CommonThreadPool {
      * @param stmt 说明文字
      */
     public static synchronized void execute(Runnable r, String stmt) {
-        synchronized (MUTEX) {
-            comment = stmt;
+        synchronized (MUTEX_EXECUTE) {
+            synchronized (MUTEX) {
+                comment = stmt;
+            }
+
+            threadPool.execute(r);
         }
-        
-        threadPool.execute(r);
     }
     
     public static void shutdown() {
